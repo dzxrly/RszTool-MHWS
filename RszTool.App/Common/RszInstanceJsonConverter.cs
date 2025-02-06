718d03d1e7c5bc2056043f0cdf9a79fcc5fd9527
dzxrly2 <dzxrly2@gmail.com>
2025-02-06 17:32:33 +0800

    更新MHWS专用版本

---
+++ RszTool.App/Common/RszInstanceJsonConverter.cs
@@ -0,0 +1,221 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Numerics;
+
+namespace RszTool.App.Common
+{
+    public class RszInstanceJsonConverter : JsonConverter<RszInstance>
+    {
+        private HashSet<int> processedInstances = new();
+
+        public override RszInstance? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException("反序列化暂不支持");
+        }
+
+        public override void Write(Utf8JsonWriter writer, RszInstance value, JsonSerializerOptions options)
+        {
+            processedInstances.Clear();
+            WriteInstance(writer, value);
+        }
+
+        private void WriteInstance(Utf8JsonWriter writer, RszInstance instance)
+        {
+            writer.WriteStartObject();
+            
+            writer.WriteString("name", instance.Name);
+            writer.WriteNumber("index", instance.Index);
+            writer.WriteNumber("objectTableIndex", instance.ObjectTableIndex);
+            writer.WriteString("className", instance.RszClass.name);
+
+            writer.WritePropertyName("fields");
+            writer.WriteStartArray();
+
+            for (int i = 0; i < instance.Fields.Length; i++)
+            {
+                WriteField(writer, instance.Fields[i], instance.Values[i]);
+            }
+
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+        }
+
+        private void WriteField(Utf8JsonWriter writer, RszField field, object value)
+        {
+            writer.WriteStartObject();
+            writer.WriteString("name", field.name);
+            writer.WriteString("type", field.DisplayType);
+            
+            writer.WritePropertyName("value");
+            if (field.array)
+            {
+                WriteArrayValue(writer, field, value);
+            }
+            else if (field.IsReference)
+            {
+                if (value is RszInstance refInstance)
+                {
+                    WriteReferenceInstanceValue(writer, refInstance);
+                }
+                else
+                {
+                    writer.WriteNullValue();
+                }
+            }
+            else
+            {
+                WriteNormalValue(writer, field, value);
+            }
+            
+            writer.WriteEndObject();
+        }
+
+        private void WriteArrayValue(Utf8JsonWriter writer, RszField field, object value)
+        {
+            writer.WriteStartArray();
+            if (value is List<object> list)
+            {
+                foreach (var item in list)
+                {
+                    if (field.IsReference && item is RszInstance instance)
+                    {
+                        WriteReferenceInstanceValue(writer, instance);
+                    }
+                    else
+                    {
+                        WriteNormalValue(writer, field, item);
+                    }
+                }
+            }
+            writer.WriteEndArray();
+        }
+
+        private void WriteReferenceInstanceValue(Utf8JsonWriter writer, RszInstance instance)
+        {
+            writer.WriteStartObject();
+            
+            // 写入引用信息
+            writer.WriteString("referenceType", "instance");
+            writer.WriteString("name", instance.Name);
+            writer.WriteNumber("index", instance.Index);
+
+            // 直接写入引用实例的字段值
+            if (instance.Fields.Length > 0)
+            {
+                writer.WritePropertyName("fields");
+                writer.WriteStartArray();
+                
+                for (int i = 0; i < instance.Fields.Length; i++)
+                {
+                    WriteField(writer, instance.Fields[i], instance.Values[i]);
+                }
+                
+                writer.WriteEndArray();
+            }
+
+            writer.WriteEndObject();
+        }
+
+        private void WriteNormalValue(Utf8JsonWriter writer, RszField field, object value)
+        {
+            switch (field.type)
+            {
+                case RszFieldType.String:
+                    writer.WriteStringValue((string)value);
+                    break;
+                case RszFieldType.Data:
+                    writer.WriteStringValue(Convert.ToBase64String((byte[])value));
+                    break;
+                case RszFieldType.Vec2:
+                case RszFieldType.Float2:
+                case RszFieldType.Point:
+                    var vec2 = (Vector2)value;
+                    writer.WriteStartObject();
+                    writer.WriteNumber("x", vec2.X);
+                    writer.WriteNumber("y", vec2.Y);
+                    writer.WriteEndObject();
+                    break;
+                case RszFieldType.Vec3:
+                case RszFieldType.Float3:
+                    var vec3 = (Vector3)value;
+                    writer.WriteStartObject();
+                    writer.WriteNumber("x", vec3.X);
+                    writer.WriteNumber("y", vec3.Y);
+                    writer.WriteNumber("z", vec3.Z);
+                    writer.WriteEndObject();
+                    break;
+                case RszFieldType.Vec4:
+                case RszFieldType.Float4:
+                    var vec4 = (Vector4)value;
+                    writer.WriteStartObject();
+                    writer.WriteNumber("x", vec4.X);
+                    writer.WriteNumber("y", vec4.Y);
+                    writer.WriteNumber("z", vec4.Z);
+                    writer.WriteNumber("w", vec4.W);
+                    writer.WriteEndObject();
+                    break;
+                case RszFieldType.Bool:
+                    writer.WriteBooleanValue((bool)value);
+                    break;
+                case RszFieldType.S8:
+                    writer.WriteNumberValue((sbyte)value);
+                    break;
+                case RszFieldType.U8:
+                    writer.WriteNumberValue((byte)value);
+                    break;
+                case RszFieldType.S16:
+                    writer.WriteNumberValue((short)value);
+                    break;
+                case RszFieldType.U16:
+                    writer.WriteNumberValue((ushort)value);
+                    break;
+                case RszFieldType.S32:
+                    writer.WriteNumberValue((int)value);
+                    break;
+                case RszFieldType.U32:
+                    writer.WriteNumberValue((uint)value);
+                    break;
+                case RszFieldType.F32:
+                    writer.WriteNumberValue((float)value);
+                    break;
+                case RszFieldType.S64:
+                    writer.WriteNumberValue((long)value);
+                    break;
+                case RszFieldType.U64:
+                    writer.WriteNumberValue((ulong)value);
+                    break;
+                case RszFieldType.F64:
+                    writer.WriteNumberValue((double)value);
+                    break;
+                default:
+                    writer.WriteStringValue(value.ToString());
+                    break;
+            }
+        }
+    }
+
+    public static class RszInstanceExtensions
+    {
+        public static string ToJson(this RszInstance instance)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Converters = { new RszInstanceJsonConverter() }
+            };
+
+            return JsonSerializer.Serialize(instance, options);
+        }
+
+        public static string ToJson(this IEnumerable<RszInstance> instances)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Converters = { new RszInstanceJsonConverter() }
+            };
+
+            return JsonSerializer.Serialize(instances, options);
+        }
+    }
+}
--- RszTool.App/ViewModels/UserFileViewModel.cs
+++ RszTool.App/ViewModels/UserFileViewModel.cs
@@ -1,3 +1,10 @@
+using Microsoft.Win32;
+using RszTool.App.Common;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Input;
+
 namespace RszTool.App.ViewModels
 {
     public class UserFileViewModel(UserFile file) : BaseRszFileViewModel
@@ -20,6 +27,37 @@ namespace RszTool.App.ViewModels
                 yield return new TreeItemViewModel("Resources", UserFile.ResourceInfoList);
                 yield return new TreeItemViewModel("Instances", RszViewModel.Instances);
                 yield return new TreeItemViewModel("Objects", RszViewModel.Objects);
+            }
+        }
+
+        private RelayCommand? exportJsonCommand;
+        public ICommand ExportJsonCommand => exportJsonCommand ??= new RelayCommand(_ => ExportJson());
+
+        private void ExportJson()
+        {
+            try
+            {
+                var saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "JSON文件|*.json",
+                    Title = "保存JSON文件",
+                };
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    var options = new JsonSerializerOptions
+                    {
+                        WriteIndented = true,
+                        Converters = { new RszInstanceJsonConverter() }
+                    };
+
+                    string json = JsonSerializer.Serialize(RszViewModel.Objects, options);
+                    System.IO.File.WriteAllText(saveFileDialog.FileName, json);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"导出JSON时发生错误：{ex.Message}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
